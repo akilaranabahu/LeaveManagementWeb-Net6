3135c0714965340745809039b00c6a3d79a02a9a
akilaranabahu <akilaranabahu@gmail.com>
2023-05-12 10:53:20 +0530

    Modified crud operation

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{  
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number of Days")]
+        [Required]
+        [Range(1, 25,ErrorMessage = "Please enter avalid number")]
+        public int DefaultDays { get; set; }
+
+    }
+}
